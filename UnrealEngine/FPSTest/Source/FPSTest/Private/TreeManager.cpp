f147cfecbff7f0edccb2c1f601a542d18335c5ab
VI0L3TF0X <mattex15243@gmail.com>
2022-12-06 22:53:16 +0100

    remove unnecessary cast, re-generate  VS files

--- UnrealEngine/FPSTest/Source/FPSTest/Private/TreeManager.cpp
+++ UnrealEngine/FPSTest/Source/FPSTest/Private/TreeManager.cpp
@@ -31,7 +31,7 @@ void UTreeManager::Initialize(int(&permutation)[256], FVector terrainPosition, F
 		position.Y *= terrainSize.Y;
 		position += terrainPosition;
 		position.Z = PerlinNoise::Noise(position.X / terrainSize.X, position.Y / terrainSize.Y, permutation) * 500;
-		AGenericTree* tree = Cast<AGenericTree>(GetWorld()->SpawnActor<AGenericTree>(treeClasses[stream.RandRange(0, 0)], FTransform(position)));
+		AGenericTree* tree = GetWorld()->SpawnActor<AGenericTree>(treeClasses[stream.RandRange(0, 0)], FTransform(position));
 		if (tree) {
 			Trees.Add(tree);
 		}
