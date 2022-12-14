b33c96e04f9a493c012819e0f67bd564cce32763
Daerjon <01151573@pw.edu.pl>
2022-12-14 18:05:38 +0100

    zalążek zwierzątek

---
+++ UnrealEngine/FPSTest/Source/FPSTest/Private/AnimalMenager.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AnimalMenager.h"
+
+// Sets default values
+AAnimalMenager::AAnimalMenager()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AAnimalMenager::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AAnimalMenager::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
